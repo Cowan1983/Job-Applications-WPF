af0837dbacabb02d4eb82a1cc427687f469e84c5
Cowan1983 <edward.stuart44@yahoo.co.uk>
2017-09-20 15:34:37 +0100

    Post Tab control additions. Pre Data Model Adaptions
    Successfully implemented the Job Leads Control being dynamically added
    to the main window Tab control. No other controls added yet.
    Pre adapting the class definitions to try to get it to use the existing
    JobLeadData database.

---
+++ Job Applications WPF/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job_Applications_WPF
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+
+        public ObservableCollection<TabItem> Tabs { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Tabs = new ObservableCollection<TabItem>();
+
+            //By default add a JobLeadListControl to the tab control
+            Tabs.Add(new TabItem { Header = "Job Leads", Type = TabItem.TabItemType.JobLeadGrid, ViewModel = new JobLeadsViewModel() });
+
+            //TO DO - ADD BROKER AND CONTACT LIST CONTROLS
+
+        }
+
+
+        public void AddNewTab()
+        {
+
+        }
+
+        public void RemoveTab()
+        {
+
+        }
+
+    }
+}
